09a2d7c98c482fc9de1f3e940d68468e87612327
Sammy <sammy.cullinan@gmail.com>
2020-10-20 15:41:30 -0700

    deploy

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
 
-// set token secret and expiration date
 const secret = "mysecretsshhhhh";
 const expiration = "2h";
 
@@ -9,25 +8,22 @@ module.exports = {
     // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // separate "Bearer" from "<tokenvalue>"
+    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(" ").pop().trim();
     }
 
-    // if no token, return request object as is
     if (!token) {
       return req;
     }
 
     try {
-      // decode and attach user data to request object
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log("Invalid token!");
+      console.log("Invalid token");
     }
 
-    // return updated request object
     return req;
   },
   signToken: function ({ username, email, _id }) {
